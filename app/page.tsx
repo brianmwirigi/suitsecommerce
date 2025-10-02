f7f78122ad1fc1d47127ab48e97474e7ba5b745b
Brian Mwirigi <brianmwirigi@Brians-MacBook-Pro.local>
2025-10-02 22:15:05 +0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-
 import React from "react";
 
 //arrow function
-const Home=()=>{
-  return ( <div>
-  <h1>Welcome to Home</h1>
-  
-    
-    </div> );
-}
- 
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+        deleniti laudantium, labore nisi earum quibusdam quisquam beatae ullam
+        consectetur incidunt modi cum dicta quaerat! Sint reprehenderit ad
+        suscipit enim nam!
+      </p>
+    </div>
+  );
+};
+
 //passin name of the function
 export default Home;
 //function declaration

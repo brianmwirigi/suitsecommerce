6c6bc1207ad56c84257aab9fa3b98f18de0e6fcd
brianmwirigi <bmmaingi17@gmail.com>
2025-10-07 22:27:43 +0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/Header";
 
 
 export const metadata: Metadata = {
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-poppins antialiased">{children}</body>
+      <body className="font-poppins antialiased">
+       <Header /> 
+       {children}
+       </body>
     </html>
   );
 }
